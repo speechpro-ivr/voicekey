f61127da7558e5383dd98d240c1b51e0560326a4
Vlad Maraev <vlad.maraev@me.com>
2012-09-18 18:11:40 +0400

    record element; junk deleted

--- lib/modules/voicekey.rb
+++
@@ -1,8 +0,0 @@
-module VoiceKey
-  class EnrollRequest
-    attr_accessor :responce
-    include HTTParty
-    format :xml
-    base_uri = 'http://voicekey.speechpro.com/avis/vk_api2/enroll.php'.
-  end
-end
