11b9bf3f9ad96108aa9001a864928270df41c71a
Vlad Maraev <vlad.maraev@me.com>
2012-09-19 10:03:12 +0400

    return rest xml

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -47,9 +47,11 @@ class ClientsController < ApplicationController
       if @client.save
         format.html { redirect_to @client, notice: 'Client was successfully created.' }
         format.json { render json: @client, status: :created, location: @client }
+        format.xml { render xml: @client, status: :created, location: @client }
       else
         format.html { render action: "new" }
         format.json { render json: @client.errors, status: :unprocessable_entity }
+        format.xml { render xml: @client, status: :created, location: @client }
       end
     end
   end
